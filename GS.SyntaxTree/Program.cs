c715d3111503d0d6c93c43fda5a27d8e80615aa6
Claudiu Epure <claudiu.epure@heg.com>
2015-05-29 14:47:44 +0300

    Getting Started with Syntax Tree

---
+++ GS.SyntaxTree/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace GS.SyntaxTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = CSharpSyntaxTree.ParseText(
+@"using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(""Hello, World!"");    
+        }
+
+        int Main(int x)
+        {
+            //Nothing
+        }  
+
+        int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}");
+            // Manually traversing the tree
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+            var firstMember = root.Members[0];
+            var helloWorldDeclaration = (NamespaceDeclarationSyntax)firstMember;
+            var programDeclaration = (ClassDeclarationSyntax)helloWorldDeclaration.Members[0];
+            var mainDeclaration = (MethodDeclarationSyntax)programDeclaration.Members[0];
+            var argsParameter = mainDeclaration.ParameterList.Parameters[0];
+
+            // Using query methods
+            var firstParameters = from methodDeclaration in root.DescendantNodes()
+                                                                    .OfType<MethodDeclarationSyntax>()
+                                  where methodDeclaration.Identifier.ValueText == "Main"
+                                  select methodDeclaration.ParameterList.Parameters.First();
+            var argsParameter2 = firstParameters.FirstOrDefault();
+
+            var collector = new UsingCollector();
+            collector.Visit(root);
+
+            foreach (var directive in collector.Usings)
+                Console.WriteLine(directive.Name);
+        }
+    }
+}
---
+++ GS.SyntaxTree/UsingCollector.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GS.SyntaxTree
+{
+    class UsingCollector : CSharpSyntaxWalker
+    {
+        public readonly IList<UsingDirectiveSyntax> Usings = new List<UsingDirectiveSyntax>();
+
+        public override void VisitUsingDirective(UsingDirectiveSyntax node)
+        {
+            if (node.Name.ToString() != "System" && !node.Name.ToString().StartsWith("System."))
+                Usings.Add(node);
+        }
+    }
+}
