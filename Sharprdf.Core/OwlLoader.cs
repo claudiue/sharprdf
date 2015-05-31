2ec87ce81762d50af55ae8babebae12b67eaba90
Claudiu Epure <claudiu.epure@gmail.com>
2015-05-31 09:02:29 +0300

    Add Sharprdf.Core.Tests project

---
+++ Sharprdf.Core/OwlLoader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VDS.RDF;
+
+namespace Sharprdf.Core
+{
+    public class OwlLoader
+    {
+        public void LoadOwl()
+        {
+            IGraph g = new Graph();
+            g.LoadFromFile("scro.owl");
+        }
+    }
+}
